07aa588248d67d2cc700cb336a2386ca105f20b4
Caroline Simon <caroline.simon@zurich.ch>
2025-11-26 13:50:18 +0100

    add filter and sort mechanism

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -2,12 +2,15 @@ import {Component, inject, OnInit} from '@angular/core';
 import {FeedbackService} from '../service/feedback.service';
 import {FeedbackOverviewComponent} from '../feedback-overview/feedback-overview.component';
 import {FeedbackDataModel} from '../../../model/feedbackData.model';
+import {FeedbackFilterComponent} from '../feedback-filter/feedback-filter.component';
+import {FilterValuesModel} from '../../../model/filtervalues.model';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
   imports: [
-    FeedbackOverviewComponent
+    FeedbackOverviewComponent,
+    FeedbackFilterComponent
   ],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
@@ -16,6 +19,7 @@ export class FeedbackComponent implements OnInit {
 
   private service = inject(FeedbackService);
   allFeedbacks:FeedbackDataModel[]= []
+  filteredFeedbacks:FeedbackDataModel[]= []
 
   ngOnInit() {
     this.loadFeedbacks();
@@ -25,7 +29,39 @@ export class FeedbackComponent implements OnInit {
     console.log("going to load feedbacks");
     this.service.getFeedbacks().subscribe(feedbacks => {
       this.allFeedbacks = feedbacks;
+      this.filteredFeedbacks = feedbacks;
       console.log("feedbacks:", feedbacks);
     })
   }
+
+  onFilterChange(filterValues: FilterValuesModel): void {
+    this.filteredFeedbacks = this.filterFeedbacks(filterValues);
+  }
+
+  filterFeedbacks(filter: FilterValuesModel): FeedbackDataModel[] {
+    let result = this.allFeedbacks.filter(f => {
+      const matchesRating = filter.rating === "ALL" || f.rating === +filter.rating;
+      const matchesProduct = filter.product === "ALL" || f.productCategory === filter.product;
+
+      const created = new Date(f.createdAt).getTime();
+      const dateFrom = filter.dateFrom ? new Date(filter.dateFrom).getTime() : null;
+      const dateTo = filter.dateTo ? new Date(filter.dateTo).getTime() : null;
+
+      const matchesDate =
+        (!dateFrom || created >= dateFrom) &&
+        (!dateTo || created <= dateTo);
+
+      return matchesRating && matchesProduct && matchesDate;
+    });
+
+    if (filter.dateSort === "DESC") {
+      result = result.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+    } else {
+      result = result.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+    }
+
+    return result;
+  }
+
+
 }
